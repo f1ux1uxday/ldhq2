efa27f7302341c1f4768c2da71620ec5f5347a2a
Lucas Dayton <lucas.dayton@zaneray.com>
2025-03-10 12:19:14 -0600

    pull in blog stories for landing page

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -4,10 +4,21 @@
 
 <script setup>
 const config = useRuntimeConfig();
+const storyblokApi = useStoryblokApi();
 const story = await useStoryblok(
   'blog-landing',
   {
     version: config.public.docVersion
   }
 )
+
+const data = await storyblokApi.getAll(
+  `cdn/stories`,
+  {
+    version: 'draft',
+    content_type: 'blog'
+  }
+)
+
+console.log(data);
 </script>
--- storyblok/Blog.vue
+++ storyblok/Blog.vue
@@ -1,13 +1,15 @@
 <template>
-  <layout-container layout-width="text">
-    <h1>{{ blok. title }}</h1>
-  </layout-container>
-  <div v-editable="blok">
-    <StoryblokComponent
-      v-for="blok in blok.blocks"
-      :key="blok._uid"
-      :blok="blok"
-    />
+  <div class="blog-post">
+    <layout-container layout-width="text">
+      <h1>{{ blok.title }}</h1>
+    </layout-container>
+    <div v-editable="blok">
+      <StoryblokComponent
+        v-for="blok in blok.blocks"
+        :key="blok._uid"
+        :blok="blok"
+      />
+    </div>
   </div>
 </template>
 
@@ -17,3 +19,9 @@ defineProps({
   story: Object
 })
 </script>
+
+<style scoped>
+.blog-post {
+  min-height: 100vh;
+}
+</style>
